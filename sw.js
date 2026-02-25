727803366b224c9d1ee478d7d81ad33c616213e4
giancop92-art <giancop92@gmail.com>
2026-02-25 23:48:38 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'chef-academy-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Installazione: salva i file nella memoria del telefono
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Gestione richieste: se non c'è internet, usa la cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
